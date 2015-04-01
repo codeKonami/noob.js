7771b77fb67f1a06bda78cf5c4f6da488cf7a19d
codeKonami <thomas.foricher@gmail.com>
2015-04-01 07:59:44 +0200

    Adding 404 page

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var express = require('express'),
     path = require('path'),
     engine = require('ejs-locals');
 
-
 var app = express();
 
 app.engine('ejs', engine);
@@ -24,6 +23,21 @@ app.use(express.cookieParser('ZsWqeEaB86vcs0qvHDq34WU3T8X7OVuU'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next){
+  res.status(404);
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 /**
  * Routes
@@ -31,8 +45,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', controllers.index);
 app.get('/contact', controllers.contact);
-// Must always remain the last route
-//app.get('*', controllers.notfound);
 
 var PORT = process.env.PORT || 3000;
 
